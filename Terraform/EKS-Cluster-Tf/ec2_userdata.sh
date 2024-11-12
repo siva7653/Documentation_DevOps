ecae67b7f431db30847d41705070522201064bce
SivaVanga <sivavanga21@gmail.com>
2024-11-12 16:04:40 +0530

    updated

--- Terraform/EKS-Cluster-Tf/ec2_userdata.sh
+++
@@ -1,8 +0,0 @@
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.3/2024-04-19/bin/linux/amd64/kubectl
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.3/2024-04-19/bin/linux/amd64/kubectl.sha256
-sha256sum -c kubectl.sha256
-openssl sha1 -sha256 kubectl
-chmod +x ./kubectl
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
-echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
-kubectl version --client
